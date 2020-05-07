65d410622e654dbb103f31a29e45e3b7cf27c7f9
Gamaliel Sánchez <gamaliel.sanchez.ext@geodis.com>
2020-05-07 18:34:44 -0500

    Second commit

--- src/app.js
+++ src/app.js
@@ -13,15 +13,6 @@ import 'react-dates/initialize';
 
 const store = configureStore();
 
-store.dispatch(addExpense({descriptions: 'Water bill',amount:4500}));
-store.dispatch(addExpense({descriptions: 'Gas bill',createdAt:1000,amount:45}));
-store.dispatch(addExpense({descriptions: 'rent',amount:67600}));
-
-
-const state = store.getState();
-const VisibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//console.log(VisibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
