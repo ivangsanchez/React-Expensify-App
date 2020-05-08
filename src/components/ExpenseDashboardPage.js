374e34626ac17b558a07ff6a97504e3c9b25d6d6
Gamaliel Sánchez <gamaliel.sanchez.ext@geodis.com>
2020-05-08 13:36:19 -0500

    Add expense summary to dasboard

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExponseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpensesSummary />
         <ExponseListFilters/>
         <ExpenseList/>
     </div>
